09b61f346c13cfeedabc7e415ecc6e6d56520c8e
Eric Frankel <efrankel624@gmail.com>
2017-08-01 12:21:00 -0700

    commenting made pretty thorough comments and an explainer at top of file also modified the digitalWrite for the anodes to reflect PMOS driver design

--- src/arduino/cube.cpp
+++ src/arduino/cube.cpp
@@ -1,47 +1,43 @@
 #include "Arduino.h"
 
-byte stateBuffer[8];
+/*
+ * This is a multiplexing driver for a 4x4x4 LED cube. Electrically, the cube is set up
+ * as an 8x8 grid. The data sent by the computer over serial to set the state is arranged such that
+ * every increment of 16bits represents an incremented x value. Every bit increment represents an 
+ * increment in y, every 4 bit increment represents an increment in z. The most significant bit 
+ * (LED 63) is stored in the 7th bit of state[7] (0-indexed). 
+ * Pins 2-9 are the anodes of the LED cube, and pins 10-17 are the cathodes. 
+ * Pins 13-17 are labeled as A0-A4 on the Arduino.
+ */
+
+
+byte state[8];//store the state of all the LEDs, state[7] bit 7 is (4,4,4)
 void display();
+
 void setup() {
     //initialize pins 2-18 to outputs
     for (int i = 2; i < 18; i++){
         pinMode(i, OUTPUT);
     }
-    /*pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
-    pinMode(8, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
-    pinMode(14, OUTPUT);
-    pinMode(15, OUTPUT);
-    pinMode(16, OUTPUT);
-    pinMode(17, OUTPUT);*/
-    Serial.begin(115200);
+    Serial.begin(115200); //set the baudrate and open port
 }
 
 void loop(){
+    //the CV controller won't send data if there is no change
     if (Serial.available() > 0){//read new data if it's available
-        Serial.readBytes(stateBuffer, 8);
+        Serial.readBytes(state, 8);
     }
-    /*for (int i = 7; i >= 0; i--){
-        Serial.print(stateBuffer[2]);
-    }*/
-    display();
+    display();//run one TDM cycle
 }
 
 void display(){
     for (int i = 2; i < 10; i++){//loop through anodes
-        digitalWrite(i, HIGH);
+        digitalWrite(i, LOW);//driver is a PMOS, so we need to write LOW to turn it on
         for (int j = 10; j < 18; j++){//loop through cathodes
-            digitalWrite(j, !(stateBuffer[i-2] & (1 << (j - 10))));//writing low turns LED on
+            byte cathodeState = state[i-2] & (1 << (j - 10))//use and to figure out if cathod j should be on
+            //0 evaultes to false (LOW), so !0 will be true (HIGH)
+            digitalWrite(j, !cathodeState);//writing low turns LED on
         }
-        digitalWrite(i, LOW);
+        digitalWrite(i, HIGH);//"turn off" anode
     } 
 }
